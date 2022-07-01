6a59c5dba9f354b62ad3d86379fad2a9127e3319
ethanloewen <ethanloewen@gmail.com>
2022-07-01 06:32:03 -0700

    Start working on mouse tracking

---
+++ src/components/Guide.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import './Guide.scss';
+
+export default function JamesBond() {
+
+  return (
+    <div className='guide'>
+      <h1>Scroll to Begin</h1>
+    </div>
+  );
+}
--- src/components/JamesBond.js
+++ src/components/JamesBond.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import './JamesBond.scss';
+import Guide from './Guide';
 
 export default function JamesBond() {
   const [circleSize, setCircleSize] = useState(50);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
+    window.addEventListener('mousemove', handleMouse);
   });
 
   const totalComponentHeight = 1000;
@@ -13,6 +15,18 @@ export default function JamesBond() {
   const handleScroll = (e) => {
     const offset = window.pageYOffset / totalComponentHeight;
     setCircleSize(offset * 5000);
+  }
+
+  const handleMouse = (e) => {
+    console.log(e.clientX / window.innerWidth);
+    console.log(e.clientY / window.innerHeight);
+
+    console.log(
+      "Window Width", window.innerWidth,
+      "Window Height", window.innerHeight,
+      "clientX: ", e.clientX, 
+      "clientY:", e.clientY
+    )
   }
 
   const circleStyle = {
